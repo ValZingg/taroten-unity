895a801ca2cbf02946de5220a4f75dfbcc4aa0f3
ValZ <Valentin.ZINGG@cpnv.ch>
2020-12-16 21:32:56 +0100

    Initial commit

---
+++ Assets/Scripts/LoadChar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadChar : MonoBehaviour
+{
+    public string[] lines;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lines = System.IO.File.ReadAllLines("Assets/Text/Maya.txt"); //Gets all lines
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //===================================
+    public float HP;
+    public float max_HP;
+    //===================================
+
+    Player(float hp)
+    {
+        max_HP = hp;
+        HP = max_HP;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
